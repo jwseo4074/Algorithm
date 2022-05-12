601d12d621ce10171235fe6bc59b47d4b017f616
zwon2056 <zwon2056@gmail.com>
2022-05-12 12:51:25 +0900

    5/12 12:51

--- BOJ/1004.py
+++ BOJ/1004.py
@@ -1,13 +1,26 @@
+# 입력의 첫 줄에는 테스트 케이스의 개수 T가 주어진다. 
+# 그 다음 줄부터 각각의 테스트케이스에 대해 첫째 줄에 출발점 (x1, y1)과 도착점 (x2, y2)이 주어진다. 
+# 두 번째 줄에는 행성계의 개수 n이 주어지며, 세 번째 줄부터 n줄에 걸쳐 행성계의 중점과 반지름 (cx, cy, r)이 주어진다.
 
-t = int(input())
-for _ in range(t):
-    cnt = 0
-    x1,y1,x2,y2 = map(int, input().split())
-    n =int(input())
-    for __ in range(n):
-        cx,cy,cr = map(int, input().split())
-        d1 = ( (x1-cx)**2 + (y1-cy)**2 )**0.5
-        d2 = ( (x2-cx)**2 + (y2-cy)**2 )**0.5
-        if (d1 > cr and d2 < cr) or (d1 < cr and d2 > cr):
-            cnt += 1
-    print(cnt)
+answerList = []
+for _ in range(int(input())):
+    x1, y1, x2, y2 = map(int, input().split())
+    # print("x1, y1, x2, y2 = ", x1, y1, x2, y2 );
+    
+    answer = 0
+
+    for i in range(int(input())):
+
+        cx, cy, r = map(int, input().split())
+
+        # print("cx, cy, r = ", cx, cy, r)
+
+        if x1 <= cx-r < x2 and x2 < cx+r:
+            answer += 1
+        if cx-r < x1 and x1 < cx+r <= x2:
+            answer += 1
+
+    answerList.append(answer)
+
+for a in answerList:
+    print(a)
