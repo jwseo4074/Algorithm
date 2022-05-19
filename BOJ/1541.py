56ebafbc86c1c4d548f61a27e905d671266e36f8
zwon2056 <zwon2056@gmail.com>
2022-05-19 16:37:09 +0900

    5/19 16:37

--- BOJ/1541.py
+++ BOJ/1541.py
@@ -1,30 +1,24 @@
 inputList = input().split('-')
-# 55-50+40
 
-# 55 , 50+40
+# print(inputList)
 
-# -를 경계로 나누니까 묶음은 무조건 +고 이걸 괄호 씌워서 -로 만들면 최소값이 될 수밖에 없다. 
+totalNumList = []
 
-numList = []
-
-for i in range(len(inputList)):
-    sumOfNum = 0
-
-    if i == 0:
-        numList.append(inputList[i])
-        continue
-
-    exceptPlusList = inputList[i].split('+')
-
-    for oneNum in exceptPlusList:
-        sumOfNum += int(oneNum)
-
-    numList.append(sumOfNum)
-
-startVal = numList[0]
-
-for i in range(1, len(numList)):
-    startVal -= int(numList[i])
-
-print(startVal)
+for word in inputList:
+    sumVal = 0
+    numList = word.split('+')
     
+    for i in range(len(numList)):
+        sumVal += int(numList[i])
+    
+    totalNumList.append(sumVal)
+    # print(sumVal)
+
+answer = 0
+for i in range(len(totalNumList)):
+    if i == 0:
+        answer = totalNumList[i]
+    else:
+        answer -= totalNumList[i]
+print(answer)
+
