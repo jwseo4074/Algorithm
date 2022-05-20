32a6bd4213c01a8a7766435c2e8e2454974c8662
zwon2056 <zwon2056@gmail.com>
2022-05-20 19:02:04 +0900

    5/20 19:02 solved not yet

--- BOJ/16235.py
+++ BOJ/16235.py
@@ -7,7 +7,6 @@
 # 2 1 3
 # 3 2 3
 
-
 energy = []
 tree = []
 plusEnergy = []
@@ -33,14 +32,50 @@ for i in range(M):
     tree[row-1][col-1].append(value)
     # 입력이 2 1 3 이면 tree[1][0] = 3 
 
-print(tree)
+# print(max(tree[1][0]))
 
 
-# for i in range(K):
+def printTreeFunc():
+    print("Tree")
+    for i in range(N):
+        for j in range(N):
+            print(tree[i][j], end = ' ')
+        print()
+
+def printEnergyFunc():
+    print("Energy")
+    for i in range(N):
+        for j in range(N):
+            print(energy[i][j], end = ' ')
+        print()
+
+for i in range(K):
     # K년 동안
 
     # 봄
+    for row in range(N):
+        for col in range(N):
+            if not tree[row][col]:
+                # 나무가 없으면 바로 재껴
+                continue
+            
+            if energy[row][col] < max(tree[row][col]):
+                tree[row][col].remove(max(tree[row][col]))
+            # 양분이 부족하면 나무 죽어
 
+            else:
+                # 양분 있으면 나이만큼 줄이고 나이 + 1 
+
+                maxIndex = tree[row][col].index(max(tree[row][col]))
+                # 여러 나무들이 모여있을 때 최대값이 있는 인덱스는?
+
+                energy[row][col] -= tree[row][col][maxIndex]
+                # 나이 증가해주기 전에 있는 양분에서 나이만큼 줄여야지
+                
+                tree[row][col][maxIndex] += 1
+                
+    printTreeFunc()
+    printEnergyFunc()
     
     # 여름
 
