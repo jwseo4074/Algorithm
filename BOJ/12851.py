b3320afa56c2e6bbdc02ccdcec076956b10d934f
zwon2056 <zwon2056@gmail.com>
2022-06-06 18:58:57 +0900

    6/6 19:00

--- BOJ/12851.py
+++ BOJ/12851.py
@@ -1,4 +1,4 @@
-from collections import deque, defaultdict
+from collections import deque
 import sys
 input = sys.stdin.readline
 
@@ -6,36 +6,47 @@ N, M = map(int, input().split())
 MAX = 100000
 
 visit = [0 for i in range(0, MAX+1)]
+# print("visit = ", visit[100000])
 
+answerTime = -1
 answerCnt = 0
-answerTime = 0
 
 def bfs():
-    global answerCnt
     global answerTime
+    global answerCnt
 
     queue = deque()
     queue.appendleft((N, 0))
 
     while(queue):
-        # input()
-        # print("Queue = ", queue)
+        input()
         X, nowtime = queue.pop()
+        print("X = ", X ," time = ", nowtime, " 방문 체크")
         visit[X] = 1
-        # print("visit[X] = ", visit[X])
-        # X 꺼내서 방문체크
 
         if X == M:
-            # print(nowtime if N!=M else 0)
-            answerCnt += 1
-            answerTime = nowtime
-            continue
+            print("찾았다")
+            if answerTime == -1: # 처음 찾았을 때 => 최단 시간
+                answerTime = nowtime
 
-        else:
-            for nx in [X-1, X+1, 2*X]:
-                if (0 <= nx <= MAX) and (visit[nx] == 0 or visit[nx] == visit[X] + 1):
-                    queue.appendleft((nx, nowtime+1))
+            if answerTime == nowtime: # 또 찾았을 때, 근데 같은 시간임
+                answerCnt += 1
+                print("AAA")
+                continue
+            else :
+                # 시간 더 오래 걸린거는 필요없어
+                print("BBB")
+                break
+        
+        print("visit Map")
+        for i in range(1, 10):
+            print("i = ", i, " ", visit[i])
+
+        for nx in [X-1, X+1, 2*X]:
+            if 0 <= nx <= MAX and visit[nx] == 0:
+                queue.appendleft((nx, nowtime+1))
+                print("queue = ", queue)
 
 bfs()
-print(answerTime)
+print(answerTime if N!=M else 0)
 print(answerCnt)
