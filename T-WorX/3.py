2f09a3eae0397934b849c0617c88941ebff41310
zwon2056 <zwon2056@gmail.com>
2022-06-13 16:19:06 +0900

    6/13 16:20

--- T-WorX/3.py
+++ T-WorX/3.py
@@ -1,60 +1,73 @@
-def solution(grid, k):
-    answer = -1
+# n = 5
+# plans = ['100 1 3', '500 4', '2000 5']
+# clients = ['300 3 5', '1500 1', '100 1 3', '50 1 2']
+# results = [3, 3, 1, 0]
 
-    dx = [1, 0, -1, 0]
-    dy = [0, -1, 0, 1]
 
-    N = len(grid)
-    M = len(grid[0])
+n = 4
+plans = ['38 2 3', '394 1 4']
+clients = ['10 2 3', '300 1 2 3 4', '500 1']
+# results = [3, 3, 1, 0]
+
+
+def solution(n, plans, clients):
+    # n = 부가서비스의 수
+    # plans = 제공 데이터, 추가 부가서비스
+    # clients = 이용 데이터, 이용 부가서비스 
+
+    results = [0 for i in range(len(clients))]
+
+    convert_p = [[] for _ in range(len(plans))]
+    convert_s = [[] for _ in range(len(clients))]
+
+    B = []
+    for i in range(len(plans)):
+        A = plans[i].split()
+        # print("A = ", A)
+        # A =  ['100', '1', '3']
+        B = B + A[1:]
+        # print(B)
+        convert_p[i].append(A[0])
+        convert_p[i].append(B)
+        # print(convert_p)
+        # [['100', ['1', '3']], ['500', ['1', '3', '4']], ['2000', ['1', '3', '4', '5']]]
     
-    visitMap = [ [0]*M for i in range(N)]
-    # print(visitMap)
+    for i in range(len(clients)):
+        # print(clients[i])
+        # 300 3 5
+        A = clients[i].split()
+        # print("A = ", A)
+        # A =  ['100', '1', '3']
+        C = A[1:]
+        # print(B)
+        convert_s[i].append(A[0])
+        convert_s[i].append(C)
+        # print(convert_s)
+        # [['300', ['3', '5']], ['1500', ['1']], ['100', ['1', '3']], ['50', ['1', '2']]]
     
-    def dfs(row, col, energy):
-        global answer 
+    for i in range(len(convert_s)):
+        for j in range(len(convert_p)):
+            # print(convert_s[i][0])
+            # print(convert_p[j][0])
+            if int(convert_s[i][0]) > int(convert_p[j][0]):
+                # print("데이터가 모자라")
+                continue
+            else:
+                if i == 2:
+                    print(j)
+                    print(convert_s[i][1])
+                    print(convert_p[j][1])
 
-        if (row == N-1 and col == M-1) or visitMap[row][col] == 1:
-            # 도착했으면 끝
-            return
-        
-        if energy == 1 and grid[row][col] == ".":
-            # 이 조건은 나중에 다시 생각하자. 에너지는 1 남았는데(한번 밖에 못가는데?) 그 곳이 평지가 아니면 어카노, 그 뒤에 평지까지 한참 남으면?
-            energy += k
-            answer += 1
-            return
+                status = True
+                for k in range(len(convert_s[i][1])):
+                    if convert_s[i][1][k] not in convert_p[j][1]:
+                        status = False
 
-        energy -= 1
-        # 에너지 하나 쓴다
-        # 강만 아니면 갈 수 있다.
+                if status:
+                    # print("찾았다")
+                    results[i] = j+1
+                    break
 
-        for i in range(4):
-            nx = col + dx[i]
-            ny = row + dy[i]
-            
-            if 0 <= nx < M and 0 <= ny < N and grid[nx][ny] != "#":
-            # # 가려는 곳이 범위 내에 있는 곳이고, 벽이 아닌 경우
+    return results
 
-                if visitMap[nx][ny] == 0:
-                    # 방문 안한 곳인 경우
-                    visitMap[nx][ny] = 1
-                    # 방문 체크
-
-        
-    dfs(0,0,0)
-
-    return answer
-
-grid = ["..FF", "###F", "###."]
-print(solution(grid, 4))
-
-
-# ".F.FFFFF.F"
-# ".########."
-# ".########F"
-# "...######F"
-# "##.######F"
-# "...######F"
-# ".########F"
-# ".########."
-# ".#...####F"
-# "...#......"
+print(solution(5, plans, clients))
